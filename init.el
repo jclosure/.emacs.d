695c410421bf988dcaed38d12ac101ecdd0697c9
Joel Holder <jclosure@gmail.com>
2015-05-26 10:25:56 -0500

    updated milkbox.net to melpa.org package repo

--- init.el
+++ init.el
@@ -110,7 +110,9 @@
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives
-	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "http://melpa.org/packages/") t)
+;(add-to-list 'package-archives
+;	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (when (< emacs-major-version 24)
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 
