5e2f5103534ca6ab8de3b4f6a5dc4a14c73d6387
Joel Holder <jclosure@gmail.com>
2015-10-01 23:02:26 -0500

    added hide-ctrl-M function to handle ^M in org-mode babel

--- init.el
+++ init.el
@@ -761,3 +761,8 @@
 ; (require 'oneonone)
 ; (1on1-emacs)
 
+(defun hide-ctrl-M ()
+  "Hides the disturbing '^M' showing up in files containing mixed UNIX and DOS line endings."
+  (interactive)
+  (setq buffer-display-table (make-display-table))
+  (aset buffer-display-table ?\^M []))
