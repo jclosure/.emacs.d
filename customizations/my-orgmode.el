9c483cb397ce790346e5b58ed92341caddf5bf96
Joel Holder <jclosure@gmail.com>
2015-10-02 20:00:34 -0500

    fixes conflict between org-mode and windmove

--- customizations/my-orgmode.el
+++ customizations/my-orgmode.el
@@ -68,5 +68,9 @@
 
 (add-hook 'org-mode-hook 'hide-ctrl-M)
 
-
+ ;; Make windmove work in org-mode:
+(add-hook 'org-shiftup-final-hook 'windmove-up)
+(add-hook 'org-shiftleft-final-hook 'windmove-left)
+(add-hook 'org-shiftdown-final-hook 'windmove-down)
+(add-hook 'org-shiftright-final-hook 'windmove-right)
 
