55c56267fcf47dabe79957c89e5877fc1256bd15
Joel Holder <jclosure@gmail.com>
2015-10-07 15:28:38 -0500

    adds cross-language block calling example in org mode

---
+++ org/auto/python.el
@@ -0,0 +1,34 @@
+(TeX-add-style-hook
+ "python"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "latin1") ("fontenc" "T1") ("ulem" "normalem")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "fixltx2e"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec:orgheadline1"
+    "sec:orgheadline5"
+    "sec:orgheadline2"
+    "tab:orgtable1"
+    "sec:orgheadline3"
+    "tab:orgtable2"
+    "sec:orgheadline4")))
+
