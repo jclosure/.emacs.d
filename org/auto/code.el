b6c147600aecf57e3ad1dc53d9b2f3b0a291f3d0
Joel Holder <jclosure@gmail.com>
2015-10-22 01:12:21 -0500

    installed lisp and slime.  adds instructions for setting it up in windows

---
+++ org/auto/code.el
@@ -0,0 +1,61 @@
+(TeX-add-style-hook
+ "code"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "fixltx2e"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec:orgheadline4"
+    "sec:orgheadline1"
+    "sec:orgheadline2"
+    "sec:orgheadline3"
+    "sec:orgheadline10"
+    "sec:orgheadline5"
+    "sec:orgheadline7"
+    "sec:orgheadline6"
+    "sec:orgheadline8"
+    "sec:orgheadline9"
+    "sec:orgheadline11"
+    "sec:orgheadline15"
+    "sec:orgheadline12"
+    "sec:orgheadline13"
+    "sec:orgheadline14"
+    "sec:orgheadline23"
+    "sec:orgheadline16"
+    "sec:orgheadline17"
+    "sec:orgheadline18"
+    "sec:orgheadline19"
+    "sec:orgheadline20"
+    "tab:orgtable1"
+    "sec:orgheadline21"
+    "sec:orgheadline22"
+    "sec:orgheadline30"
+    "sec:orgheadline24"
+    "sec:orgheadline25"
+    "sec:orgheadline26"
+    "sec:orgheadline27"
+    "sec:orgheadline28"
+    "sec:orgheadline29"
+    "sec:orgheadline33"
+    "sec:orgheadline31"
+    "sec:orgheadline32")))
+
